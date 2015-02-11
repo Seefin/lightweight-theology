6ab2a2c300ee05e28c433a5ddffc3343e1ef207c
Connor Findlay <connor.findlay@ecs.vuw.ac.nz>
2015-02-11 19:57:04 +1300

    Make the controller an ActionListener. The controller is now responsible for responding to UI events, not the model. This more closely represents the spirit of the MVC model.

--- leightweight-theology/src/PlayerController.java
+++ leightweight-theology/src/PlayerController.java
@@ -1,5 +1,9 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
-public class PlayerController {
+
+public class PlayerController implements ActionListener{
 	PlayerView view;
 	PlayerModel model;
 	
@@ -7,9 +11,29 @@ public class PlayerController {
 		model = new PlayerModel("test.wav");
 		view = new PlayerView();
 		
-		view.addStopListener(model);
-		view.addPlayListener(model);
-		view.addBrowseListener(model);
+		view.addStopListener(this);
+		view.addPlayListener(this);
+		view.addBrowseListener(this);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getActionCommand().equalsIgnoreCase("browse")){
+			try {
+				File file = view.browseFile();
+				model.setFilename(file);
+			} catch (Exception err) {
+				err.printStackTrace();
+			}
+		} else if(e.getActionCommand().equalsIgnoreCase("play")){
+			System.out.println("Play!");
+			model.playFile();
+		} else if(e.getActionCommand().equalsIgnoreCase("stop")){
+			System.out.println("Stop!");
+			model.stopFile();
+		} else {
+			System.out.println(e.getActionCommand() + " not implemented");
+		}
 	}
 	
 	public static void main(String[] args) {
