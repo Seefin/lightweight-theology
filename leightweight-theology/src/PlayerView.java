6eba55cd8bc9fbe60714ed8d4527f2e2ac0bcd53
Connor Findlay <connor.findlay@ecs.vuw.ac.nz>
2015-02-11 20:02:52 +1300

    Refactored code. Added label update functionality, the ability to select a song, and a filter on the files visable.

--- leightweight-theology/src/PlayerView.java
+++ leightweight-theology/src/PlayerView.java
@@ -2,18 +2,36 @@ import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
-import javax.swing.*;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.filechooser.FileNameExtensionFilter;
+/**Represents the view in the MVC model of the lightweight-theology application.
+ * This class is the view; it is purely a GUI for the application, and holds no internal
+ * logic. 
+ * @author Connor Findlay
+ */
 public class PlayerView extends JFrame {
+	//Constants
+	private static final String FileLabelPrefix = "Now Playing: ";
 	private static final long serialVersionUID = 1L;
-	JTextField filename;
-	JButton openFile,stop,start;
-	JPanel viewPanel;
-	
-	public static void main(String args[]){
-		new PlayerView();
-	}
+	//UI elements
+	private JLabel filename;
+	private JButton openFile,stop,start;
+	private JPanel viewPanel;
+	private String filenameString = "test.wav";
 
+	/**Construct a new view.
+	 * Constructs a new view object for the lightweight-theology application. Basically
+	 * Initializes the User Interface.
+	 */
 	public PlayerView(){
 		//Get the UI set up
 		initUI();
@@ -27,7 +45,7 @@ public class PlayerView extends JFrame {
 		stop.setActionCommand("stop");
 		stop.addActionListener(listener);
 	}
-	
+
 	/**Add an ActionListener to the start component.
 	 * Adds to the actionListeners of the start button.
 	 * @param listener The component to set as the ActionListener
@@ -36,7 +54,7 @@ public class PlayerView extends JFrame {
 		start.setActionCommand("play");
 		start.addActionListener(listener);
 	}
-	
+
 	/**Add an ActionListener to the browse component.
 	 * Adds to the actionListeners of the browse button.
 	 * @param listener The component to set as the ActionListener
@@ -45,9 +63,46 @@ public class PlayerView extends JFrame {
 		openFile.setActionCommand("browse");
 		openFile.addActionListener(listener);
 	}
-	
+
+	/**Choose a file to play.
+	 * I am unsure if this should be in the model; most of it is fiddling with JFileChoosers.
+	 * 
+	 * @throws IOException If we somehow select a non-existent file.
+	 * @throws LineUnavailableException If there are no sourcelines to play the file chosen.
+	 * @throws UnsupportedAudioFileException If the file chosen is not formatted correctly.
+	 */
+	public File browseFile() throws IOException, UnsupportedAudioFileException, LineUnavailableException {
+		JFileChooser fc = new JFileChooser();
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("WAV Files", "wav","WAV");
+		fc.addChoosableFileFilter(filter);
+		fc.setFileFilter(filter);
+		int retVal = fc.showOpenDialog(null);
+		if(retVal == JFileChooser.APPROVE_OPTION){
+			File f = fc.getSelectedFile();
+			if(!f.exists()){throw new IOException();}
+			updateFilenameString(f.getName());
+			//Revalidates everything so the JFrame resizes to new string size.
+			viewPanel.revalidate();
+			this.revalidate();
+			this.pack();
+			return f;
+		} else {
+			return null;
+		}
+	}
+
 	//------ Privates ------
-	
+
+	/**Update the filename string.
+	 * Updates the string for the filename, and then sets the JLabel text
+	 * to the new filename.
+	 * @param name The name of the currently playing file.
+	 */
+	private void updateFilenameString(String name) {
+		filenameString = name;
+		filename.setText(FileLabelPrefix + filenameString);
+	}
+
 	/**Initialize the view.
 	 * This method initializes and populates the view with the
 	 * buttons and fields required.
@@ -56,19 +111,19 @@ public class PlayerView extends JFrame {
 		//Set frame default behaviors and layout
 		setLayout(new FlowLayout());
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
+
 		//Initialize UI elements
-		filename = new JTextField(20);
+		filename = new JLabel(FileLabelPrefix + filenameString );
 		openFile = new JButton("Browse...");
-		stop = new JButton("Stop");
+		stop = new JButton("Pause");
 		start = new JButton("Play");
 		viewPanel = new JPanel(new GridBagLayout());
-		
-		
+
+
 		//Place UI elements
 		// This is complex because of the gridbaglayout, so it's in
 		// it's own method.
-		arrangeUIelements();
+		arrangeUIElements();
 		//Add component panel
 		add(viewPanel);
 		//Show the view window
@@ -81,32 +136,41 @@ public class PlayerView extends JFrame {
 	 * because of the large amount of configuration needed for each element,
 	 * it makes more sense in it's own method.
 	 */
-	private void arrangeUIelements() {
+	private void arrangeUIElements() {
 		//Initialize display area.
 		GridBagConstraints gbc = new GridBagConstraints();
-		
+
 		//Add filename field
-		gbc.anchor = GridBagConstraints.WEST;
+		gbc.anchor = GridBagConstraints.BASELINE;
 		gbc.gridx = 0;
 		gbc.gridy = 0;
 		gbc.weightx = 0.8;
 		gbc.weighty = 0.5;
+		gbc.ipadx = 6;
 		viewPanel.add(filename, gbc);
 
 		//Add Browse button
 		gbc.gridx = 1;
 		gbc.weightx = 0.2;
 		viewPanel.add(openFile, gbc);
-		
+
 		//Add Play button
 		gbc.gridy = 1;
 		gbc.gridx = 0;
 		gbc.weightx = 0.1;
 		viewPanel.add(start, gbc);
-		
+
 		//Add Stop button
 		gbc.gridx = 1;
 		gbc.anchor = GridBagConstraints.EAST;
 		viewPanel.add(stop, gbc);
+	}
+
+	/**For testing ONLY
+	 * 
+	 * @param args
+	 */
+	public static void main(String args[]){
+		new PlayerView();
 	}
 }
