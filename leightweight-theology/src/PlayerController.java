ea8e5306933221eb515a276880f04ac3543a66ab
Connor Findlay <connor.findlay@ecs.vuw.ac.nz>
2015-02-12 15:40:12 +1300

    Refactored and commented. Added explinations to the ActionListener method, and refactored some code. Also added in the automatic playback of a file when the user hits ok from the browse window.

--- leightweight-theology/src/PlayerController.java
+++ leightweight-theology/src/PlayerController.java
@@ -6,12 +6,18 @@ import java.io.IOException;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
-
-public class PlayerController implements ActionListener{
+public class PlayerController implements ActionListener {
+	//Model and View Components.
 	PlayerView view;
 	PlayerModel model;
 
-	public PlayerController(){
+	/**
+	 * Construct a new controller instance. As the controller constructs the
+	 * model and view instances, this effectivly starts the program; we make
+	 * this file the listener for the various buttons, then hand control off to
+	 * the user.
+	 */
+	public PlayerController() {
 		model = new PlayerModel("test.wav");
 		view = new PlayerView();
 
@@ -20,41 +26,84 @@ public class PlayerController implements ActionListener{
 		view.addBrowseListener(this);
 	}
 
+	/**
+	 * ActionListener. This method is effectivly the `event-driven' part of the
+	 * program; when a user presses a button, the view notifies the controller
+	 * via this method, and then the controller does the required action.
+	 * 
+	 * @param event
+	 *            An event object containing information about what happened to
+	 *            trigger this method.
+	 * 
+	 */
 	@Override
-	public void actionPerformed(ActionEvent e) {
-		if(e.getActionCommand().equalsIgnoreCase("browse")){
+	public void actionPerformed(ActionEvent event) {
+		/*
+		 * Handles the browse button. When the user presses the browse button,
+		 * the controller asks the view to display a dialog and browse for the
+		 * file, then tell us which file the user wants. The controller uses
+		 * this to tell the model which file we now want to play. We then play
+		 * the file, and notify the view that we are now in the 'playing' state.
+		 */
+		if (event.getActionCommand().equalsIgnoreCase("browse")) {
 			try {
 				File file = view.browseFile();
 				model.setFilename(file);
-				view.pauseFile(true);
+				model.playFile();
+				view.pauseFile(false);
 			} catch (Exception err) {
 				err.printStackTrace();
 			}
-		} else if(e.getActionCommand().equalsIgnoreCase("play")){
+		}
+		/*
+		 * Handle the play button. When the user presses the play button, we
+		 * tell the model to play the file, and then tell the view to toggle the
+		 * play/pause button to act as a pause button.
+		 */
+		else if (event.getActionCommand().equalsIgnoreCase("play")) {
 			model.playFile();
 			view.pauseFile(false);
-		} else if(e.getActionCommand().equalsIgnoreCase("stop")){
-			try{
-				//Rewind the file
-				model.stopFile();
-				//Stop playback and reset play button state
+		}
+		/*
+		 * Handle the stop button. When the user presses stop, we tell the model
+		 * to pause playback (so we don't have any funny issues) and the view to
+		 * reset the play/pause toggle to the play state. We then tell the model
+		 * to stop the file, and reset it to the beginning.
+		 */
+		else if (event.getActionCommand().equalsIgnoreCase("stop")) {
+			try {
+				// Stop playback and reset play button state
 				model.pauseFile();
 				view.pauseFile(true);
+				// Rewind the file
+				model.stopFile();
 			} catch (IOException err) {
 				System.out.println("Problem reading the selected file");
 				err.printStackTrace();
-			} catch (LineUnavailableException err){
+			} catch (LineUnavailableException err) {
 				System.out.println("The Audio System has no Line avaliable");
 				err.printStackTrace();
-			} catch (UnsupportedAudioFileException err){
+			} catch (UnsupportedAudioFileException err) {
 				System.out.println("The Audio file is not supported");
 				err.printStackTrace();
 			}
-		} else if(e.getActionCommand().equalsIgnoreCase("pause")){
+		}
+		/*
+		 * Handle the pause button. When the user presses the pause button, we
+		 * tell the model to pause playback, and the view the file is no paused,
+		 * so the toggle state is set correctly.
+		 */
+		else if (event.getActionCommand().equalsIgnoreCase("pause")) {
 			model.pauseFile();
 			view.pauseFile(true);
-		} else {
-			System.out.println(e.getActionCommand() + " not implemented");
+		}
+		/*
+		 * Handle any other button. If we start receiving events we shouldn't,
+		 * we just state the function is not implemented in this version of the
+		 * player.
+		 */
+		else {
+			System.out.println(event.getActionCommand() + " not implemented");
 		}
 	}
 
