c29ebe7530a85ee8d66cc80224848f9312541f1e
Connor Findlay <connor.findlay@ecs.vuw.ac.nz>
2015-02-12 14:33:00 +1300

    Added stop functionality and refactored. This commit adds in stop button functionality to the program; this is achieved by unloading and then reloading all the audio resources when the `reset` flag is set. This reinitialization is not resource intensive, so it is a safe way of handling individual files stopping and starting. Fixes #1.

--- leightweight-theology/src/PlayerModel.java
+++ leightweight-theology/src/PlayerModel.java
@@ -13,66 +13,59 @@ import javax.sound.sampled.SourceDataLine;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class PlayerModel {
-	//Data
+	// Data
 	private File audioFile;
 	private AudioFormat format;
-	private BufferedInputStream bis;
+	private volatile BufferedInputStream bis;
 	private AudioInputStream stream;
 	private SourceDataLine dataline;
-	private volatile boolean stop = false; 
+	private volatile boolean stop = false;
 	private volatile boolean reset = false;
 	private ExecutorService es = Executors.newCachedThreadPool();
 
-	//Calculation
-	/**Creates a new Model.
-	 * The created model has all of the audio system set up and ready to go, and
-	 * just needs to be supplied with an audio file so it can play it.
-	 * @param filename The filename of the audio file to play.
+	// Calculation
+	/**
+	 * Creates a new Model. The created model has all of the audio system set up
+	 * and ready to go, and just needs to be supplied with an audio file so it
+	 * can play it.
+	 * 
+	 * @param filename
+	 *            The filename of the audio file to play.
 	 */
-	public PlayerModel(String filename){
+	public PlayerModel(String filename) {
 		try {
 			audioFile = new File(filename);
 			setupAudioSystem();
 		} catch (Exception e) {
 			e.printStackTrace();
-		} 
+		}
 	}
 
-	/**Play the selected file.
-	 * Plays the selected file. It does this by wrapping all the 'play stream' logic in
-	 * an anonymous instance of Runnable, and uses java's concurrency libraries to get it running.
+	/**
+	 * Play the selected file. Plays the selected file. It does this by wrapping
+	 * all the 'play stream' logic in an anonymous instance of Runnable, and
+	 * uses java's concurrency libraries to get it running.
 	 * 
-	 * As the stop boolean is volatile, it will be monitored even in a multiple core environment. 
+	 * As the stop boolean is volatile, it will be monitored even in a multiple
+	 * core environment.
 	 */
-	public void playFile(){
-		//Set up for play back
+	public void playFile() {
+		// Set up for play back
 		stop = false;
 		reset = false;
 		byte tempBuffer[] = new byte[10000];
-		//Mark the beginning of the stream. Make it valid for the whole song.
-		bis.mark(Integer.MAX_VALUE);
-		//Wrap play logic in a runnable
-		Runnable playTask = new Runnable(){
-			public void run(){
+		// Wrap play logic in a runnable
+		Runnable playTask = new Runnable() {
+			public void run() {
 				try {
 					dataline.open(format);
 					dataline.start();
 					int cnt = 0;
-					while ((cnt = stream.read(tempBuffer, 0,
-							tempBuffer.length)) != -1 && !stop) {
-						if(reset){
-							System.out.println("Theoretical reset");
-							bis.reset();
-							cnt = 0;
-//							//Close off all things
-//							dataline.drain();
-//							dataline.close();
-//							//Open them again
-//							dataline.open(format);
-//							dataline.start();
-//							stream.reset();
-//							//Reset reset
-							reset = false;
+					while ((cnt = stream.read(tempBuffer, 0, tempBuffer.length)) != -1
+							&& !stop) {
+						if (reset) {
+							System.out.println("reset == true");
+							return;
 						}
 						if (cnt > 0) {
 							dataline.write(tempBuffer, 0, cnt);
@@ -86,51 +79,75 @@ public class PlayerModel {
 				}
 			}
 		};
-		//Run the runnable somewhere asynchronously.
+		// Run the runnable somewhere asynchronously.
 		es.execute(playTask);
 	}
 
-	/**Stops playback of the current file.
+	/**
+	 * Stops playback of the current file.
 	 * 
 	 */
-	public void pauseFile(){
+	public void pauseFile() {
 		stop = true;
 	}
 
-	/**Resets the file to the beginning.
-	 * This method is effectively a 'rewind' method. It resets the stream to the beginning,
-	 * and then plays the stream.
+	/**
+	 * Resets the file to the beginning. This method is effectively a 'rewind'
+	 * method. It resets the stream to the beginning, and then plays the stream.
+	 * 
+	 * @throws LineUnavailableException
+	 *             If we can't get an Audio Line
+	 * @throws UnsupportedAudioFileException
+	 *             The Audio file is *wrong*
+	 * @throws IOException
+	 *             There was an IO related error in reading the file
 	 */
-	public void stopFile(){
+	public void stopFile() throws IOException, UnsupportedAudioFileException,
+			LineUnavailableException {
+		// Signal to play back mechanism we want to not play back
 		reset = true;
+		// Reset back to the intial state
+		setupAudioSystem();
 	}
 
-
-	/**Set up the system for playing audio.
-	 * We initialize the stream, grab the format of the file (should be 16-bit signed Low-endian PCM wav), and
+	/**
+	 * Set up the system for playing audio. We initialize the stream, grab the
+	 * format of the file (should be 16-bit signed Low-endian PCM wav), and
 	 * finally grab a line so we can play the file itself.
-	 * @throws IOException	If the file is non-existent
-	 * @throws UnsupportedAudioFileException If the format is not supported on this hardware
-	 * @throws LineUnavailableException If there are no source lines for the program to use.
+	 * 
+	 * @throws IOException
+	 *             If the file is non-existent
+	 * @throws UnsupportedAudioFileException
+	 *             If the format is not supported on this hardware
+	 * @throws LineUnavailableException
+	 *             If there are no source lines for the program to use.
 	 */
-	private void setupAudioSystem() throws IOException, UnsupportedAudioFileException, LineUnavailableException {
-		if(!audioFile.exists()){System.out.println("Not exist!");throw new IOException();}
-		//To enable marking and reseting the stream, we wrap in a buffered stream.
+	private void setupAudioSystem() throws IOException,
+			UnsupportedAudioFileException, LineUnavailableException {
+		if (!audioFile.exists()) {
+			System.out.println("Not exist!");
+			throw new IOException();
+		}
+		// To enable marking and reseting the stream, we wrap in a buffered
+		// stream.
 		bis = new BufferedInputStream(new FileInputStream(audioFile));
 		stream = AudioSystem.getAudioInputStream(bis);
 		format = stream.getFormat();
 		dataline = AudioSystem.getSourceDataLine(format);
 	}
 
-	/**Set the file to a new thing!
-	 * Allows dynamic changing of the file to be played by the user.
-	 * @param args File to change it to.
+	/**
+	 * Set the file to a new thing! Allows dynamic changing of the file to be
+	 * played by the user.
+	 * 
+	 * @param args
+	 *            File to change it to.
 	 */
 	public void setFilename(File file) {
-		if(file != null){
-			stopFile();
-			audioFile = file;
+		if (file != null) {
 			try {
+				stopFile();
+				audioFile = file;
 				setupAudioSystem();
 			} catch (Exception e) {
 				e.printStackTrace();
@@ -140,14 +157,14 @@ public class PlayerModel {
 		}
 	}
 
-	public static void main(String args[]){
+	public static void main(String args[]) {
 		PlayerModel pm = new PlayerModel("test.wav");
 		pm.playFile();
-		try{
+		try {
 			Thread.sleep(3000);
-		} catch(Exception e){
+			pm.stopFile();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		pm.stopFile();
 	}
 }
